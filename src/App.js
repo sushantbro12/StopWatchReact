9e5d06794030552c24db8e713089f7c8fa123bae
sushantbro12 <sushantbro121738@gmail.com>
2024-11-11 20:05:33 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const CountdownTimer = () => {
+  const [time, setTime] = useState(1500);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timeInterval;
+
+    if (isRunning && time > 0) {
+      timeInterval = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, [isRunning, time]);
+
+  const handleStart = () => {
+    setIsRunning((prev) => !prev);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(1500);
+  };
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const doubleSecondDigits =
+    String(seconds).length === 1 ? `0` + seconds : seconds;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen bg-red-500">
+      <div className="flex flex-col items-center justify-center bg-red-400 p-20">
+        <div>
+          <h1 className="text-9xl font-bold text-white mb-5">
+            {minutes}:{doubleSecondDigits}
+          </h1>
+        </div>
+        <div className="flex gap-10">
+          <button
+            className="bg-white text-red-500 font-bold rounded-sm p-3 "
+            onClick={handleStart}
+          >
+            {isRunning ? "Stop" : "Start"}
+          </button>
+
+          <button
+            className="bg-white text-red-500 font-bold rounded-sm p-3"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CountdownTimer;
